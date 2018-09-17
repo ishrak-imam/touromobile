319d28e339e49ae42874a1e9d2b13aa0c8741f4c
Ishrak <ishrak@cefalo.com>
2018-09-17 16:18:24 +0600

    .toJS() removed from getPax selector

--- app/selectors/trip.js
+++ app/selectors/trip.js
@@ -1,10 +1,10 @@
 
-import { getMap } from '../utils/immutable'
+import { setIntoMap } from '../utils/immutable'
 
 export const getCurrentTrip = state => state.currentTrip // state.get('currentTrip')
 
 export const getPax = bookings => {
   return bookings.map(b => {
-    return b.get('pax').map(p => getMap({ ...p.toJS(), booking: b }))
+    return b.get('pax').map(p => setIntoMap(p, 'booking', b))
   }).flatten(1) // one level deep flatten
 }
