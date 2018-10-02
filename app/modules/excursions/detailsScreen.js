00b5ac3d9e03b8e59bb3e7ea66ed68d003435793
Loke Dahlström <loke.dahlstrom@gmail.com>
2018-10-02 15:22:54 +0200

    getItemLayout test

--- app/modules/excursions/detailsScreen.js
+++ app/modules/excursions/detailsScreen.js
@@ -108,8 +108,13 @@ class ExcursionDetailsScreen extends Component {
           immutableData={pax}
           renderItem={this._renderItem(participants)}
           keyExtractor={this._keyExtractor}
-          windowSize={3}
           initialNumToRender={20}
+          getItemLayout={(data, index) => ({
+              length: 53,
+              offset: 53 * index,
+              index
+            })
+          }
           />
         : <NoData text='No match found' textStyle={{ marginTop: 30 }} />
     )
