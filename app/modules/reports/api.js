28f8185c24f8217fc94e20f993b6cab39b423533
Ishrak <ishrak@cefalo.com>
2019-02-11 17:06:53 +0600

    guideId added in food order stats endpoint

--- app/modules/reports/api.js
+++ app/modules/reports/api.js
@@ -12,11 +12,11 @@ export const uploadStats = (guideId, departureId, statsData, jwt) => {
     : postRequest(`resources/departure/${departureId}/guide/${guideId}/statistics`, statsData, headers)
 }
 
-export const uploadOrderStats = (departureId, OrderStats, jwt) => {
+export const uploadOrderStats = (guideId, departureId, OrderStats, jwt) => {
   const headers = {
     'Authorization': `Bearer ${jwt}`
   }
   return config.useMockData
     ? mockUploadStats()
-    : postRequest(`resources/departure/${departureId}/invoiceDetails`, OrderStats, headers)
+    : postRequest(`resources/departure/${departureId}/guide/${guideId}/invoiceDetails`, OrderStats, headers)
 }
