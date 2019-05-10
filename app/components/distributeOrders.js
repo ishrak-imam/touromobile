622e0f5f7cd3f573dc3541fbc9ebd65b73589605
Ishrak <ishrak@cefalo.com>
2019-05-10 17:47:46 +0600

    pending check method in separate file

--- app/components/distributeOrders.js
+++ app/components/distributeOrders.js
@@ -18,6 +18,7 @@ import {
   setInvoiceeList, setIsNeedDistribution
 } from '../modules/modifiedData/action'
 import { getMap, getList, listToMap } from '../utils/immutable'
+import { checkIfAllDistributed } from '../utils/checkIfAllDistributed'
 import { Colors } from '../theme'
 import OutHomeTab from '../components/outHomeTab'
 import { showModal } from '../modal/action'
@@ -26,21 +27,6 @@ import _T from '../utils/translator'
 class DistributeOrders extends Component {
   constructor (props) {
     super(props)
-    // let {
-    //   participants, extraOrders, orders,
-    //   bookingId, departureId
-    // } = props
-
-    // participants = this._flattenParticipants(participants)
-
-    // actionDispatcher(setOrderBucket({
-    //   bookingId,
-    //   departureId,
-    //   bucket: getMap({
-    //     participants, extraOrders, orders
-    //   })
-    // }))
-
     this.state = {
       tab: 'out'
     }
@@ -55,7 +41,7 @@ class DistributeOrders extends Component {
     }))
   }
 
-  _checkIfAllDistributed = invoiceeList => {
+  _setDistributionFlag = invoiceeList => {
     let { participants, orders, extraOrders } = this.props
     const bucket = getMap({
       participants: this._flattenParticipants(participants),
@@ -65,104 +51,7 @@ class DistributeOrders extends Component {
       }),
       extraOrders
     })
-
-    const distributed = invoiceeList.reduce((map, invoicee, paxId) => {
-      const extraOrders = invoicee.get('extraOrders')
-      if (extraOrders && extraOrders.size) {
-        map = map.mergeIn(['extraOrders'], extraOrders)
-      }
-
-      const participants = invoicee.get('participants')
-      if (participants && participants.size) {
-        participants.every((par, excursionId) => {
-          let exPar = map.getIn(['participants', excursionId])
-          if (!exPar) exPar = par
-          else {
-            exPar = exPar.set('count', exPar.get('count') + par.get('count'))
-          }
-          map = map.setIn(['participants', excursionId], exPar)
-          return true
-        })
-      }
-
-      const orders = invoicee.get('orders')
-      if (orders && orders.size) {
-        const outMeals = orders.get('out')
-        if (outMeals && outMeals.size) {
-          const meals = outMeals.get('meal')
-          meals.every((meal, mealId) => {
-            let bMeal = map.getIn(['orders', 'out', 'meal', mealId]) || getMap({ mealId, adultCount: 0, childCount: 0 })
-            bMeal = bMeal.set('adultCount', bMeal.get('adultCount') + meal.get('adultCount'))
-              .set('childCount', bMeal.get('childCount') + meal.get('childCount'))
-            const allergyMeals = meal.get('allergies')
-            if (allergyMeals && allergyMeals.size) {
-              allergyMeals.every((aMeal, aMealId) => {
-                let baMeal = bMeal.getIn(['allergies', aMealId]) || getMap({
-                  adult: aMeal.get('adult'),
-                  adultCount: 0,
-                  allergyId: aMealId,
-                  allergyText: aMeal.get('allergyText'),
-                  child: aMeal.get('child'),
-                  childCount: 0,
-                  mealId: aMeal.get('mealId')
-                })
-                baMeal = baMeal.set('adultCount', baMeal.get('adultCount') + aMeal.get('adultCount'))
-                  .set('childCount', baMeal.get('childCount') + aMeal.get('childCount'))
-                bMeal = bMeal.setIn(['allergies', aMealId], baMeal)
-                return true
-              })
-            }
-            map = map.setIn(['orders', 'out', 'meal', mealId], bMeal)
-            return true
-          })
-        }
-
-        const homeMeals = orders.get('home')
-        if (homeMeals && homeMeals.size) {
-          const meals = homeMeals.get('meal')
-          meals.every((meal, mealId) => {
-            let bMeal = map.getIn(['orders', 'home', 'meal', mealId]) || getMap({ mealId, adultCount: 0, childCount: 0 })
-            bMeal = bMeal.set('adultCount', bMeal.get('adultCount') + meal.get('adultCount'))
-              .set('childCount', bMeal.get('childCount') + meal.get('childCount'))
-            const allergyMeals = meal.get('allergies')
-            if (allergyMeals && allergyMeals.size) {
-              allergyMeals.every((aMeal, aMealId) => {
-                let baMeal = bMeal.getIn(['allergies', aMealId]) || getMap({
-                  adult: aMeal.get('adult'),
-                  adultCount: 0,
-                  allergyId: aMealId,
-                  allergyText: aMeal.get('allergyText'),
-                  child: aMeal.get('child'),
-                  childCount: 0,
-                  mealId: aMeal.get('mealId')
-                })
-                baMeal = baMeal.set('adultCount', baMeal.get('adultCount') + aMeal.get('adultCount'))
-                  .set('childCount', baMeal.get('childCount') + aMeal.get('childCount'))
-                bMeal = bMeal.setIn(['allergies', aMealId], baMeal)
-                return true
-              })
-            }
-            map = map.setIn(['orders', 'home', 'meal', mealId], bMeal)
-            return true
-          })
-        }
-      }
-
-      return map
-    }, getMap({
-      participants: getMap({}),
-      orders: getMap({
-        out: getMap({ meal: getMap({}) }),
-        home: getMap({ meal: getMap({}) })
-      }),
-      extraOrders: getMap({})
-    }))
-
-    return distributed.equals(bucket)
-  }
-
-  _setDistributionFlag = invoiceeList => {
-    const isAllDistributed = this._checkIfAllDistributed(invoiceeList)
+    const isAllDistributed = checkIfAllDistributed(bucket, invoiceeList)
     const { bookingId, departureId } = this.props
     actionDispatcher(setIsNeedDistribution({
       departureId,
