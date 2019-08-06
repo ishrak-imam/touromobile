2d540249811afe8563a3b525e44067607c6a8dcd
Ishrak <ishrak@cefalo.com>
2019-08-06 10:46:20 +0600

    api request documentation

--- app/modules/connectionLines/api.js
+++ app/modules/connectionLines/api.js
@@ -2,11 +2,13 @@
 import config from '../../utils/config'
 import { mockConnectionLines } from '../../mockData'
 import { getRequest } from '../../utils/request'
+// import { createDoc } from '../../../api_doc/docCreator'
 
 export const getConnectionLines = (departureId, jwt) => {
   const headers = {
     'Authorization': `Bearer ${jwt}`
   }
+  // createDoc('GET', 'resources/departure/{departureId}/connections', {}, 'getConnectionLines')
   return config.useMockData
     ? mockConnectionLines()
     : getRequest(`resources/departure/${departureId}/connections`, headers)
