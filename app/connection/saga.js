e7ef032751e506bd85d0100a80521b485041e736
Ishrak <ishrak@cefalo.com>
2019-08-22 17:20:11 +0600

    netinfo subscription method changed

--- app/connection/saga.js
+++ app/connection/saga.js
@@ -1,7 +1,11 @@
 
 import NetInfo from '@react-native-community/netinfo'
 import { call, put, takeEvery, select } from 'redux-saga/effects'
-import { takeFirst, eventEmitterChannel } from '../utils/sagaHelpers'
+import {
+  takeFirst,
+  EECforNetInfo
+  // eventEmitterChannel
+} from '../utils/sagaHelpers'
 
 import {
   checkConnection,
@@ -42,12 +46,33 @@ export function * watchConnection () {
   yield takeFirst(startConnectionMonitor.getType(), createConnectionSubscription)
 }
 
+// function * createConnectionSubscription (action) {
+//   const connectionChannel = yield call(
+//     eventEmitterChannel,
+//     NetInfo,
+//     { on: 'addEventListener', off: 'removeEventListener' },
+//     'connectionChange'
+//   )
+//   yield takeEvery(connectionChannel, function * (connection) {
+//     yield put(connectionType(connection.type))
+//     if (connection.type !== 'none') {
+//       yield put(connectionStatus(true))
+//       const user = yield select(getUser)
+//       const isLoggedIn = user.get('accessToken')
+//       if (isLoggedIn) {
+//         yield put(syncPendingProfileUpdate())
+//       }
+//     } else {
+//       yield put(connectionStatus(false))
+//     }
+//   })
+// }
+
 function * createConnectionSubscription (action) {
   const connectionChannel = yield call(
-    eventEmitterChannel,
+    EECforNetInfo,
     NetInfo,
-    { on: 'addEventListener', off: 'removeEventListener' },
-    'connectionChange'
+    { on: 'addEventListener' }
   )
   yield takeEvery(connectionChannel, function * (connection) {
     yield put(connectionType(connection.type))
